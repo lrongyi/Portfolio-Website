d25e5caaa3936e2fe7fb4d48756c265ade0ea065
lrongyi <iygnormal@gmail.com>
2025-03-13 16:17:19 +0800

    Add education animations

--- scripts/education.js
+++ scripts/education.js
@@ -1,26 +1,19 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     const educationSection = document.querySelector("#education")
-//     const childElements = document.querySelectorAll(".left-container, .right-container")
+document.addEventListener("DOMContentLoaded", () => {
+    const educationSection = document.querySelector("#education")
 
-//     educationSection.classList.remove("education-visible")
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("education-visible")
+                } else {
+                    entry.target.classList.remove("education-visible")
+                }
+            });
+        },
+        { rootMargin: "-10% 0px -10% 0px", threshold: 0.1 }
+    );
 
-//     const observer = new IntersectionObserver(
-//         (entries) => {
-//             entries.forEach(entry => {
-//                 if (entry.isIntersecting) {
-//                     childElements.forEach(child => {
-//                         child.classList.add("education-visible")
-//                     })
-//                 } else {
-//                     childElements.forEach(child => {
-//                         child.classList.remove("education-visible")
-//                     });
-//                 }
-//             });
-//         },
-//         { rootMargin: "-10% 0px -10% 0px", threshold: 0.8 }
-//     );
-
-//     educationContainers.forEach(container => observer.observe(container))
-// });
+    observer.observe(educationSection)
+});
 
